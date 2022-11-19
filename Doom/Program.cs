5628adbf553ce0afdf1030c28e18489138691714
CeniusM <93879193+CeniusM@users.noreply.github.com>
2022-11-19 23:08:44 +0100

    Save

--- Doom/Program.cs
+++ Doom/Program.cs
@@ -5,10 +5,13 @@ namespace Game;
 
 class Doom
 {
+    const int ScreenWidth = 100;
+    const int ScreenHeight = 50;
+
     static void Main()
     {
         // Init engine
-        SlackingGameEngine.SlackingGameEngine engine = new SlackingGameEngine.SlackingGameEngine(100, 50);
+        SlackingGameEngine.SlackingGameEngine engine = new SlackingGameEngine.SlackingGameEngine(ScreenWidth, ScreenHeight);
 
         engine.Start();
         engine.ShowFPS = true;
@@ -35,7 +38,8 @@ class Doom
         int MapHeight = MapConstruct.Length;
 
         const float WalkSpeed = 0.001f;
-        const float TurnSpeed = 0.01f;
+        const float TurnSpeed = 0.001f;
+        const float FOV = 1.57f; // 90 degress
         float PlayerX = 0;
         float PlayerY = 0;
         // Radiants, 0 - ~6.283
@@ -65,6 +69,11 @@ class Doom
             }
         }
 
+        // Will be a one d line that tells us how far away a wall is, 0 = very very very close, and 1 is futher away
+        // This tells us how tall to make the lines
+        // Range 0 - screen height
+        int[] VeiwLine = new int[engine.GetWidthOfBuffer()];
+
         void Move(float offSet)
         {
             float x = MathF.Cos(PlayerAngle + offSet) * engine.DeltaF * WalkSpeed;
@@ -85,6 +94,7 @@ class Doom
 
         while (true)
         {
+            string DebugMessage = "";
             engine.Update();
 
             // Game Logic
@@ -93,13 +103,13 @@ class Doom
             {
                 PlayerAngle -= TurnSpeed * engine.DeltaF;
                 if (PlayerAngle < 0)
-                    PlayerAngle += 6.9f;
+                    PlayerAngle += 6.28f;
             }
             if (engine.GetKeyState((char)39)) // right
             {
                 PlayerAngle += TurnSpeed * engine.DeltaF;
-                if (PlayerAngle > 6.9f)
-                    PlayerAngle -= 6.9f;
+                if (PlayerAngle > 6.28f)
+                    PlayerAngle -= 6.28f;
             }
 
             // Move
@@ -114,6 +124,62 @@ class Doom
 
 
 
+            // -- Render --
+            Renderer.Clear(buffer, new Pixel());
+
+            // Render world
+            // Floor
+            Renderer.RenderRect(buffer, 0, 30, ScreenWidth, 10, new Pixel(Renderer.LOW, ConsoleColor.Green, ConsoleColor.Black));
+            Renderer.RenderRect(buffer, 0, 40, ScreenWidth, 10, new Pixel(Renderer.HALF, ConsoleColor.Green, ConsoleColor.Black));
+
+            // Generate VeiwLine
+            for (ushort i = 0; i < ScreenWidth; i++)
+            {
+                // Get vector for ray
+                const float StepSize = 0.1f;
+                float xPos = PlayerX;
+                float yPos = PlayerY;
+                float xVec = MathF.Cos(PlayerAngle + FOV / ScreenWidth * (i - ScreenWidth / 2)) * StepSize;
+                float yVec = MathF.Sin(PlayerAngle + FOV / ScreenWidth * (i - ScreenWidth / 2)) * StepSize;
+
+                if (i == 0)
+                    DebugMessage += xVec.ToString();
+
+                // Cast ray and set distace value
+                const int MaxSteps = 100;
+                for (int step = 0; step < MaxSteps; step++)
+                {
+                    int tempX = (int)xPos;
+                    int tempY = (int)yPos;
+
+                    if (tempX < 0 || tempY < 0 || tempX >= MapWidth || tempY >=MapHeight)
+                        break;
+
+                    if (Map[tempX, (int)tempY] != 0)
+                    {
+                        VeiwLine[i] = MaxSteps / 4 - step;
+                        break;
+                    }
+                    else
+                    {
+                        xPos += xVec;
+                        yPos += yVec;
+                    }
+                }
+            }
+
+            // Disblay VeiwLine
+            for (ushort i = 0; i < ScreenWidth; i++)
+                if (VeiwLine[i] < 3)
+                    continue;
+                else if (VeiwLine[i] < 6)
+                    Renderer.RenderRect(buffer, i, (ushort)(25 - VeiwLine[i]), 1, (ushort)(VeiwLine[i] * 2), new Pixel(Renderer.LOW, ConsoleColor.White, ConsoleColor.Black));
+                else if (VeiwLine[i] < 12)
+                    Renderer.RenderRect(buffer, i, (ushort)(25 - VeiwLine[i]), 1, (ushort)(VeiwLine[i] * 2), new Pixel(Renderer.HALF, ConsoleColor.White, ConsoleColor.Black));
+                else if (VeiwLine[i] < 18)
+                    Renderer.RenderRect(buffer, i, (ushort)(25 - VeiwLine[i]), 1, (ushort)(VeiwLine[i] * 2), new Pixel(Renderer.HIGH, ConsoleColor.White, ConsoleColor.Black));
+                else if (VeiwLine[i] < 26)
+                    Renderer.RenderRect(buffer, i, (ushort)(25 - VeiwLine[i]), 1, (ushort)(VeiwLine[i] * 2), new Pixel(Renderer.FULL, ConsoleColor.White, ConsoleColor.Black));
 
 
             // Render map
@@ -121,14 +187,17 @@ class Doom
             {
                 Renderer.RenderText(buffer, 0, (ushort)i, DisblayMap[i], Color.GetColor(ConsoleColor.White, ConsoleColor.Black));
             }
+
             // Player
             int x = (int)PlayerX;
             int y = (int)PlayerY;
             if (x < MapWidth && x > -1 && y < MapHeight && y > -1)
                 Renderer.RenderText(buffer, (ushort)(x * 2), (ushort)y, "!!", Color.GetColor(ConsoleColor.Red, ConsoleColor.Black));
 
-            // Render world
 
+
+
+            Renderer.RenderText(buffer, 0, 20, DebugMessage, Color.GetColor(ConsoleColor.White, ConsoleColor.Black));
 
             Thread.Sleep(10);
 
