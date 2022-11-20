1d429561fba1f717b66b81355d419fa948fdcd82
CeniusM <93879193+CeniusM@users.noreply.github.com>
2022-11-20 19:56:38 +0100

    Changes

--- ColorPalette/Program.cs
+++ ColorPalette/Program.cs
@@ -14,13 +14,20 @@ IntPtr buffer = engine.GetActiveBuffer();
 
 while (true)
 {
-    for (ushort i = 0; i < 16; i++)
-    {
-        for (ushort j = 0; j < 16; j++)
-        {
-            Renderer.RenderRect(buffer, (ushort)(i * 10), (ushort)(j * 10), 16, 10, new Pixel(Renderer.HALF, (byte)i, (byte)j));
-        }
-    }
+    //for (ushort i = 0; i < 16; i++)
+    //{
+    //    for (ushort j = 0; j < 16; j++)
+    //    {
+    //        Renderer.RenderRect(buffer, (ushort)(i * 10), (ushort)(j * 10), 16, 10, new Pixel(Renderer.HALF, (byte)i, (byte)j));
+    //    }
+    //}
+
+
+    // Shades
+    Pixel[] shades = new Pixel[4 * 4 * 4];
+    // Not implement, but will contain a range of (FULL, HIGH, HALF, LOW) * ForeGround(White, Gray, DarkGray, Dark) * BackGround(White, Gray, DarkGray, Dark)
+
+
 
 
     engine.RenderBuffer();
