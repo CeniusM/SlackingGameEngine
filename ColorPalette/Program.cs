03ef00f566e3847f1823bc631fa7d1387449cae9
CeniusM <93879193+CeniusM@users.noreply.github.com>
2022-11-20 18:47:51 +0100

    Changes

---
+++ ColorPalette/Program.cs
@@ -0,0 +1,28 @@
+﻿using SlackingGameEngine;
+using SlackingGameEngine.Render;
+
+const int ScreenWidth = 160;
+const int ScreenHeight = 80;
+
+// Init engine
+SlackingGameEngine.SlackingGameEngine engine = new SlackingGameEngine.SlackingGameEngine(ScreenWidth, ScreenHeight);
+
+engine.Start();
+engine.ShowFPS = true;
+
+IntPtr buffer = engine.GetActiveBuffer();
+
+while (true)
+{
+    for (ushort i = 0; i < 16; i++)
+    {
+        for (ushort j = 0; j < 16; j++)
+        {
+            Renderer.RenderRect(buffer, (ushort)(i * 10), (ushort)(j * 10), 16, 10, new Pixel(Renderer.HALF, (byte)i, (byte)j));
+        }
+    }
+
+
+    engine.RenderBuffer();
+    Thread.Sleep(100);
+}
